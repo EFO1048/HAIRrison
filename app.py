312c0ba9c1b023105b72de5d43a878f22ed6efd6
EFOSchool <obrienef@mail.uc.edu>
2026-03-04 10:28:53 -0500

    Made radio buttons actually affect other buttons

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+import os
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from time import sleep
 
 app = Flask(__name__)
@@ -18,6 +19,15 @@ def connect():
 @app.route("/continueOn", methods=["GET"])
 def continueOn():
     return render_template("main.html")
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "logo.png",
+        mimetype="image/png",
+    )
 
 
 if __name__ == "__main__":
--- static/mainScript.js
+++ static/mainScript.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("connectBtn").addEventListener("click", testConnection);
+    // needed to separate these because btn is out of the dom
+    const connectBtn = document.getElementById("connectBtn")
+    if (connectBtn) {
+        connectBtn.addEventListener("click", testConnection);
+    }
+
+    // radio buttons for state by state or full
+    document.getElementById("fullWalkthrough").addEventListener("click",disableStateStart);
+    document.getElementById("stateByState").addEventListener("click", enableStateStart);
+
+    // full start and stop
+    document.getElementById("fullStart").addEventListener("click", function(event){
+        stateStart(event, 1)
+    });
+
+    document.getElementById("fullStop").addEventListener("click", function(event){
+        stateStart(event, 1)
+    });
+
+    // state buttons
     document.getElementById("state1").addEventListener("click", function(event){
         stateStart(event, 1)
     });
@@ -14,8 +33,43 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// enable all of the state by state buttons 
+// disable start and stop but they'd be re-enabled once
+// you choose a state later on
+function enableStateStart() {
+    // enable state buttons
+    const stateBtns = document.getElementsByClassName("stateBtn");
+    for (let i = 0; i < 4; i++) {
+        stateBtns[i].disabled = false;
+    }
+
+    // disable the start and stop
+    const startBtn = document.getElementById("fullStart");
+    const stopBtn = document.getElementById("fullStop");
+
+    startBtn.disabled = true;
+    stopBtn.disabled = true;
+}
+
+// disable all of the state buttons
+function disableStateStart() {
+    // re-enable full btns by default
+    const startBtn = document.getElementById("fullStart");
+    const stopBtn = document.getElementById("fullStop");
+
+    startBtn.disabled = false;
+    stopBtn.disabled = false;
+
+    const stateBtns = document.getElementsByClassName("stateBtn");
+    for (let i = 0; i < 4; i++) {
+        stateBtns[i].disabled = true;
+    }
+}
+
 async function stateStart(stateNum) {
     
+
+
 }
 
 async function testConnection() {
@@ -60,4 +114,13 @@ function hideSpinner() {
 // Continue to main page
 function continueOn() {
     window.location.href = "/continueOn";
+}
+
+// PROGRESS BAR AND TIMERS
+function startProgress() {
+
+}
+
+function startTimer() {
+    
 }
